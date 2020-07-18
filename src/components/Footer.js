40c601a1a0e73451f1e8e74189690fb2afeafe68
kazimakyildiz <kazimakyildiz@yandex.com>
2020-07-18 23:17:26 +0300

    vimeo delete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -75,7 +75,7 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <a title="facebook" href="https://www.facebook.com/AkyildizKazim">
                   <img
                     src={facebook}
                     alt="Facebook"
@@ -89,7 +89,7 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
+                <a title="twitter" href="https://twitter.com/akyildizkazim">
                   <img
                     className="fas fa-lg"
                     src={twitter}
@@ -97,17 +97,10 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="instagram" href="https://instagram.com">
+                <a title="instagram" href="https://www.instagram.com/kazim_akyildiz/">
                   <img
                     src={instagram}
                     alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                 <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>  
