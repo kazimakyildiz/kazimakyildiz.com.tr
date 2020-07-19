fd8ebb3ab855d5cd99a608b82a5da35404910be6
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-07-19 20:35:14 +0300

    ka_nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
-import kanav from '../img/social/kanav.svg'
+import kanav from '../img/social/ka_nav.svg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
