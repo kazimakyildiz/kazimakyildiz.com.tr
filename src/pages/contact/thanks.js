7480ab4d9f79cbd1b01f7902f975170c36e0dd4b
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-07-23 22:18:14 +0300

    contact thanks

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -6,8 +6,8 @@ export default () => (
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <h1>Mesajınız için Teşekkürler!</h1>
+          <p>En Kısa Sürede Dönüş YApmaya Çalışacağım</p>
         </div>
       </div>
     </section>
