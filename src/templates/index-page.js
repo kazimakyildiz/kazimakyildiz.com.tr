0205db61eb2ac8c7c32142807651d69458e20d9f
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-07-25 20:22:45 +0300

    1

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -56,7 +56,7 @@ export const IndexPageTemplate = ({
               'rgb(0, 0, 139) 0.5rem 0px 0px, rgb(0, 0, 139) -0.5rem 0px 0px',
             backgroundColor: 'rgb(0, 0, 139)',
             color: 'white',
-            lineHeight: '3',
+            lineHeight: '1',
             padding: '1em',
           }}
         >
