2e0b2a47580651f18c45345c11b3f4cce0b6cca2
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-07-25 21:57:31 +0300

    gri colour

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -40,7 +40,7 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(0, 0, 139) 0.5rem 0px 0px, rgb(0, 0, 139) -0.5rem 0px 0px',
+              'rgb(166, 172, 175) 0.5rem 0px 0px, rgb(166, 172, 175) -0.5rem 0px 0px',
             backgroundColor: 'rgb(0, 0, 139)',
             color: 'white',
             lineHeight: '1',
@@ -53,7 +53,7 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
             boxShadow:
-              'rgb(0, 0, 139) 0.5rem 0px 0px, rgb(0, 0, 139) -0.5rem 0px 0px',
+              'rgb(166, 172, 175 ) 0.5rem 0px 0px, rgb(166, 172, 175) -0.5rem 0px 0px',
             backgroundColor: 'rgb(0, 0, 139)',
             color: 'white',
             lineHeight: '0.75',
