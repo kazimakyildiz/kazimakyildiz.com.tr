97efda0d4dcdcafc3ece04d617bd3984d9be1be8
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-07-25 19:10:47 +0300

    column 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,16 +19,16 @@ const Footer = class extends React.Component {
             style={{ width: '14em', height: '7em' }}
           />
         </div>
-        <div className="content has-text-left has-background-black has-text-white-ter">
+        <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div style={{ maxWidth: '80vw' }} className="columns">
-              <div className="column is-4">
+              <div className="column is-2">
                 <section className="menu">
                   <ul className="menu-list">
                   </ul>
                 </section>
               </div>
-              <div className="column is-4">
+              <div className="column is-2">
                 <section>
                   <ul className="menu-list">
                     <li>
