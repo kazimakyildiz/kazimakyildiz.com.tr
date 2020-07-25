e3e16ef011097e9e1def68dd3e6f9c1f191ed9f9
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-07-25 17:09:48 +0300

    tr

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,7 +11,7 @@ const TemplateWrapper = ({ children }) => {
   return (
     <div>
       <Helmet>
-        <html lang="en" />
+        <html lang="tr" />
         <title>{title}</title>
         <meta name="description" content={description} />
 
