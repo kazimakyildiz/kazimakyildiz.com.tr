2fb3989f0a95af227382d31e61bd7b6294720fab
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-07-25 18:10:44 +0300

    footer admin and products delete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,17 +26,6 @@ const Footer = class extends React.Component {
                 <section className="menu">
                   <ul className="menu-list">
                     <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
                       <a
                         className="navbar-item"
                         href="/admin/"
