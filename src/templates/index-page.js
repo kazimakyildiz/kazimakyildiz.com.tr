d5acbc2a1f0ae4fb1b1444a8b3b1f9a9654da9e3
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-09-05 19:51:23 +0300

    export

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -125,25 +125,7 @@ export const pageQuery = graphql`
         }
         heading
         subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
+        
       }
     }
   }
