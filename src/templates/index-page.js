f97f0a35f00dde4accae3632c60da4750ca56b9c
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-09-05 19:57:15 +0300

    layout

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -93,9 +93,7 @@ const IndexPage = ({ data }) => {
         title={frontmatter.title}
         heading={frontmatter.heading}
         subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
+        
       />
     </Layout>
   )
