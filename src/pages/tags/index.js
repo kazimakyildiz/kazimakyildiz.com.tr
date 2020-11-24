4e0c0f7b9cdc90b5f66ea084b8cd6420053dc5d5
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-11-24 23:50:04 +0300

    export tags

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -39,3 +39,18 @@ const TagsPage = ({
 )
 
 export default TagsPage
+export const tagPageQuery = graphql`
+  query TagsQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(limit: 1000) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`
