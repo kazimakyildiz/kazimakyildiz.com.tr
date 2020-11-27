bc8f8d2f2a35b4357f55e6b6901fa662fd2245be
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-11-27 22:12:51 +0300

    aa

--- src/cms/preview-templates/googleadsPreview.js
+++ src/cms/preview-templates/googleadsPreview.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { BlogPostTemplate } from '../../templates/blog-post'
+import { googl-eadsTemplate } from '../../templates/googleads'
 
-const BlogPostPreview = ({ entry, widgetFor }) => {
+const googleadsPreview = ({ entry, widgetFor }) => {
   const tags = entry.getIn(['data', 'tags'])
   return (
-    <BlogPostTemplate
+    <google-adsTemplate
       content={widgetFor('body')}
       description={entry.getIn(['data', 'description'])}
       tags={tags && tags.toJS()}
@@ -14,11 +14,11 @@ const BlogPostPreview = ({ entry, widgetFor }) => {
   )
 }
 
-BlogPostPreview.propTypes = {
+googleadsPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   widgetFor: PropTypes.func,
 }
 
-export default BlogPostPreview
+export default googleadsPreview
