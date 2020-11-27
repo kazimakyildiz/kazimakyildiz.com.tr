c5300ac58f757bd13e31dc1a2e58550c4fab53b0
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-11-27 22:53:15 +0300

    delete post

--- src/pages/google-ads/index.js
+++ src/pages/google-ads/index.js
@@ -22,7 +22,7 @@ export default class BlogIndexPage extends React.Component {
               padding: '1rem',
             }}
           >
-            Son Yazılar
+            Google Ads
           </h1>
         </div>
         <section className="section">
