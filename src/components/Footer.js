7b056facaed50429370156c23d3765e5d7c0b334
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-11-28 16:43:35 +0300

    nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,7 +24,7 @@ const Footer = class extends React.Component {
             <div style={{ maxWidth: '80vw' }} className="columns">
               <div className="column is-2">
                 <section>
-                  <ul className="menu-list">
+                  <ul className="nav">
                     <li>
                       <Link className="navbar-item" to="/blog">
                         Tüm Yazılar
