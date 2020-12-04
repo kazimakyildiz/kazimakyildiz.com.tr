d879394694e732bd102724fd18fb0ab6ac5ee571
Kazım Akyıldız <kazim@UNBOXTEST.local>
2020-12-05 01:30:49 +0300

    google ads delete

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,11 +4,10 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
-import googleadsPreview from './preview-templates/googleadsPreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
-CMS.registerPreviewTemplate('googleads', googleadsPostPreview)
+
