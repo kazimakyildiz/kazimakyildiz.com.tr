4ba50a44e802ddaf8d5e292f1d1f77685302363a
kazimakyildiz <kazimakyildiz@yandex.com>
2021-01-09 17:31:05 +0300

    gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,87 +1,79 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Kazım Akyıldız | Performans Pazarlaması',
-    description:
-      'Google Ads ve Facebook Ads ve diğer reklam platformlarındaki yenilikleri ve kendi işimi kolaylaştıran araçları ve deneyimlerimi paylaşıyorum.',
+export const siteMetadata = {
+  title: 'Kazım Akyıldız | Performans Pazarlaması',
+  description: 'Google Ads ve Facebook Ads ve diğer reklam platformlarındaki yenilikleri ve kendi işimi kolaylaştıran araçları ve deneyimlerimi paylaşıyorum.',
+};
+export const plugins = [
+  'gatsby-plugin-react-helmet',
+  {
+    // keep as first gatsby-source-filesystem plugin for gatsby image support
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      path: `${__dirname}/static/img`,
+      name: 'uploads',
+    },
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/static/img`,
-        name: 'uploads',
-      },
+  {
+    resolve: 'gatsby-plugin-sass',
+    options: {
+      indentedSyntax: true
     },
-    {
-      resolve: 'gatsby-plugin-sass',
-      options: {
-        indentedSyntax: true
-      },
-    },  
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      path: `${__dirname}/src/pages`,
+      name: 'pages',
     },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
-      },
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      path: `${__dirname}/src/img`,
+      name: 'images',
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-relative-images',
-            options: {
-              name: 'uploads',
-            },
+  },
+  'gatsby-plugin-sharp',
+  'gatsby-transformer-sharp',
+  {
+    resolve: 'gatsby-transformer-remark',
+    options: {
+      plugins: [
+        {
+          resolve: 'gatsby-remark-relative-images',
+          options: {
+            name: 'uploads',
           },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
+        },
+        {
+          resolve: 'gatsby-remark-images',
+          options: {
+            // It's important to specify the maxWidth (in pixels) of
+            // the content container as this plugin uses this as the
+            // base for generating different widths of each image.
+            maxWidth: 2048,
           },
-          {
-            resolve: 'gatsby-remark-copy-linked-files',
-            options: {
-              destinationDir: 'static',
-            },
+        },
+        {
+          resolve: 'gatsby-remark-copy-linked-files',
+          options: {
+            destinationDir: 'static',
           },
-        ],
-      },
+        },
+      ],
     },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+  },
+  {
+    resolve: 'gatsby-plugin-netlify-cms',
+    options: {
+      modulePath: `${__dirname}/src/cms/cms.js`,
     },
-    {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
-      options: {
-        develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
-      },
-    }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-    
-  
-   
-  
-  ],
-    
-}
+  },
+  {
+    resolve: 'gatsby-plugin-purgecss',
+    options: {
+      develop: true,
+      purgeOnly: ['/all.sass'],
+    },
+  },
+  'gatsby-plugin-netlify',
+];
